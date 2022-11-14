38abfcc335ee39ec0919d5452f2a522cd5cbf298
Luis Morales <moralesl@users.noreply.github.com>
2022-11-14 08:03:45 +0100

    Add logs to StepFunction

--- cdk/state-management-demo-asl-stack.ts
+++ cdk/state-management-demo-asl-stack.ts
@@ -5,6 +5,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as logs from "aws-cdk-lib/aws-logs";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
 import * as sns from "aws-cdk-lib/aws-sns";
 import * as sqs from "aws-cdk-lib/aws-sqs";
@@ -24,7 +25,7 @@ export class StateManagementASLDemoStack extends cdk.Stack {
 
     const riderStateChangeEvent = new sns.Topic(this, "RiderStateChangeEvent-ASL-CDK", {
       displayName: "RiderStateChangeEvent-ASL-CDK",
-      topicName: "RiderStateChangeEvent-ASL-CDK"
+      topicName: "RiderStateChangeEvent-ASL-CDK",
     });
 
     const riderStateDLQ = new sqs.Queue(this, "RiderStateDLQ-ASL-CDK", {
@@ -55,9 +56,14 @@ export class StateManagementASLDemoStack extends cdk.Stack {
       "RiderStateTransitionManagement-ASL-CDK",
       {
         definition: new sfn.Pass(this, "StartState"),
-        tracingEnabled: true,
         stateMachineName: "RiderStateTransitionManagement-ASL-CDK",
         stateMachineType: sfn.StateMachineType.EXPRESS,
+        tracingEnabled: true,
+        logs: {
+          destination: new logs.LogGroup(this, "RiderStateTransitionManagement-ASL-CDK-LogGroup"),
+          level: sfn.LogLevel.ALL,
+          includeExecutionData: true,
+        },
       }
     );
 
